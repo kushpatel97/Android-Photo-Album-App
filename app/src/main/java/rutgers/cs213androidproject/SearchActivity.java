90ba1f3ae02b912ff335df4cd8ea367119b3056f
Kush Patel <kushp447@gmail.com>
2018-05-02 15:51:28 -0400

    Searchview

--- app/src/main/java/rutgers/cs213androidproject/SearchActivity.java
+++ app/src/main/java/rutgers/cs213androidproject/SearchActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,6 +27,7 @@ public class SearchActivity extends AppCompatActivity {
     public EditText editText;
     public CustomSpinner customSpinner;
     public GridView gridView;
+    public SearchView searchView;
 
     public ArrayList<Photo> photoList = new ArrayList<>();
     public AlbumImageAdapter albumImageAdapter;
@@ -43,11 +45,11 @@ public class SearchActivity extends AppCompatActivity {
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
 
 
-        search = (ImageButton) findViewById(R.id.searchTags);
-        editText = (EditText) findViewById(R.id.tagName);
+//        search = (ImageButton) findViewById(R.id.searchTags);
+//        editText = (EditText) findViewById(R.id.tagName);
         customSpinner = (CustomSpinner) findViewById(R.id.tagspinner);
         gridView = (GridView) findViewById(R.id.gridview_search);
-
+        searchView = (SearchView) findViewById(R.id.searchField);
         albumImageAdapter = new AlbumImageAdapter(SearchActivity.this, photoList);
         gridView.setAdapter(albumImageAdapter);
 
@@ -67,49 +69,82 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                photoList.clear();
+                if(option == 0){
+                    photoList.addAll(MainActivity.session.searchCertainTags(spinneroptions[option],s));
+                    gridView = (GridView) findViewById(R.id.gridview_search);
+                    albumImageAdapter = new AlbumImageAdapter(SearchActivity.this, photoList);
+                    gridView.setAdapter(albumImageAdapter);
+                }
+                else if(option == 1){
+                    photoList.addAll(MainActivity.session.searchCertainTags(spinneroptions[option],s));
+                    gridView = (GridView) findViewById(R.id.gridview_search);
+                    albumImageAdapter = new AlbumImageAdapter(SearchActivity.this, photoList);
+                    gridView.setAdapter(albumImageAdapter);
+                }
+                else{
+                    photoList.addAll(MainActivity.session.searchAllTags(s));
+                    gridView = (GridView) findViewById(R.id.gridview_search);
+                    albumImageAdapter = new AlbumImageAdapter(SearchActivity.this, photoList);
+                    gridView.setAdapter(albumImageAdapter);
+                }
+                return true;
+            }
+        });
+
+
+
         gridView = (GridView) findViewById(R.id.gridview_search);
         albumImageAdapter = new AlbumImageAdapter(SearchActivity.this, photoList);
         gridView.setAdapter(albumImageAdapter);
 
 
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(editText == null || editText.getText().toString().isEmpty()){
-                    AlertDialog alertDialog = new AlertDialog.Builder(SearchActivity.this).create();
-                    alertDialog.setTitle("Incomplete Field");
-                    alertDialog.setMessage("Please enter a value to be searched");
-                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            });
-                    alertDialog.show();
-                    return;
-                }
-                photoList.clear();
-                String tagkey = spinneroptions[option];
-                String tagvalue = editText.getText().toString();
-                Tag tag = new Tag(tagkey, tagvalue);
-                if(option == 0){
-                    photoList.addAll(MainActivity.session.searchCertainTags(tag.key, tag.value));
-                }
-                else if(option == 1){
-                    photoList.addAll(MainActivity.session.searchCertainTags(tag.key, tag.value));
-                }
-                else if(option == 2){
-                    photoList.addAll(MainActivity.session.searchAllTags(tag.value));
-                }
-
-
-
-                gridView = (GridView) findViewById(R.id.gridview_search);
-                albumImageAdapter.notifyDataSetChanged();
-                gridView.setAdapter(albumImageAdapter);
-
-            }
-        });
+//        search.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if(editText == null || editText.getText().toString().isEmpty()){
+//                    AlertDialog alertDialog = new AlertDialog.Builder(SearchActivity.this).create();
+//                    alertDialog.setTitle("Incomplete Field");
+//                    alertDialog.setMessage("Please enter a value to be searched");
+//                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+//                            new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int which) {
+//                                    dialog.dismiss();
+//                                }
+//                            });
+//                    alertDialog.show();
+//                    return;
+//                }
+//                photoList.clear();
+//                String tagkey = spinneroptions[option];
+//                String tagvalue = editText.getText().toString();
+//                Tag tag = new Tag(tagkey, tagvalue);
+//                if(option == 0){
+//                    photoList.addAll(MainActivity.session.searchCertainTags(tag.key, tag.value));
+//                }
+//                else if(option == 1){
+//                    photoList.addAll(MainActivity.session.searchCertainTags(tag.key, tag.value));
+//                }
+//                else if(option == 2){
+//                    photoList.addAll(MainActivity.session.searchAllTags(tag.value));
+//                }
+//
+//
+//
+//                gridView = (GridView) findViewById(R.id.gridview_search);
+//                albumImageAdapter.notifyDataSetChanged();
+//                gridView.setAdapter(albumImageAdapter);
+//
+//            }
+//        });
 
 
     }
